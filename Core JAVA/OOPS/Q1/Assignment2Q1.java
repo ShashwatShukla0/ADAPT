2495dc993b39dcf23fcf357427b0a6c04e7009db
ShashwatShukla0 <49423715+ShashwatShukla0@users.noreply.github.com>
2021-02-04 17:08:38 +0530

    Add files via upload

---
+++ Core JAVA/OOPS/Q1/Assignment2Q1.java
@@ -0,0 +1,13 @@
+package assignment.one;
+
+public class Assignment2Q1 {
+
+	public static void main(String[] args) {
+		
+		Singleton tmp = Singleton.getInstance( );
+	      tmp.demoMethod( );
+		
+
+	}
+
+}
