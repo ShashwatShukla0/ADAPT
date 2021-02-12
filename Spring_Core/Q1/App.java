b6520d396b65cc60688dcc4e0ef9f8e13936b182
ShashwatShukla0 <49423715+ShashwatShukla0@users.noreply.github.com>
2021-02-12 13:53:07 +0530

    Add files via upload

---
+++ Spring_Core/Q1/App.java
@@ -0,0 +1,22 @@
+package Q1;
+
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Q1/Spring.xml");
+        Customer customer= (Customer) context.getBean("Customer");
+        customer.getCustomerName();
+        customer.getCustomerNumber();
+        customer.getCustomerId();
+        Address address= (Address) context.getBean("Address");
+        address.getAddress();
+        
+    }
+}
