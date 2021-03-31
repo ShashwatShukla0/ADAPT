cdc46a9ef36f9c34cb3df7dbbb05ba7df071f832
ShashwatShukla0 <49423715+ShashwatShukla0@users.noreply.github.com>
2021-03-31 13:55:56 +0530

    Add files via upload

---
+++ Spring_Rest/Q2/LoginControl.java
@@ -0,0 +1,23 @@
+package Q2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/*
+ * controller class
+ */
+@Controller
+public class LoginControl {
+	
+	/*
+	 * getting the login view
+	 */
+	@RequestMapping(value="/login.html" , method = RequestMethod.GET)
+	public ModelAndView go() {
+		
+		ModelAndView m = new ModelAndView("login");
+		return m;
+	}
+}
