90db277f18e7d3dcce40054abb05c79499937f16
ShashwatShukla0 <49423715+ShashwatShukla0@users.noreply.github.com>
2021-03-31 13:46:45 +0530

    Update HomeResource.java

--- Spring_Security/Q9/HomeResource.java
+++ Spring_Security/Q9/HomeResource.java
@@ -1,4 +1,4 @@
-package io.javabrains.springsecurityjdbc;
+package Q9;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
