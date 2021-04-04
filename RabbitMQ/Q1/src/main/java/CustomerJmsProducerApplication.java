2f626b094c061b7301b19cdeb5e97ffefe799cd4
ShashwatShukla0 <49423715+ShashwatShukla0@users.noreply.github.com>
2021-04-04 21:35:20 +0530

    Add files via upload

---
+++ RabbitMQ/Q1/src/main/java/CustomerJmsProducerApplication.java
@@ -0,0 +1,13 @@
+package Q1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerJmsProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerJmsProducerApplication.class, args);
+	}
+
+}
